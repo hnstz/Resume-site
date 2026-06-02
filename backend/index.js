306c05c46a14d6f61ffcc5458c08fbccb28f9423
hnstz <hryhoryevaa.anastasiya@gmail.com>
2026-06-02 13:58:58 +0300

    import bug fix

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,13 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import nodemailer from 'nodemailer';
 import { OpenAI } from 'openai';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
